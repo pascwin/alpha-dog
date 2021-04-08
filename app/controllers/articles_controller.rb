8cfa23304815dbc4c3c960e1a374bed1b01ed198
Ubuntu <ubuntu@ip-172-31-20-249.eu-central-1.compute.internal>
2021-04-08 12:26:08 +0000

    added user-article association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was succesfully created"
             redirect_to @article
